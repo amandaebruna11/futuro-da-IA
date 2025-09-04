63a4737770381302094b7831ac277d5e0b9022e5
bruna costa <166858058+amandaebruna11@users.noreply.github.com>
2025-09-04 16:54:07 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caica-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado: ,
+        alternativas: [
+
+        ]
+    },
+    {
+        enunciado: ,
+        alternativas: [
+
+        ]
+    },
+    enunciado: ,
+    alternativas: [
+
+    ]
+    {
+        enunciado: ,
+        alternativas: [
+
+        ]
+    },
+    {
+        enunciado: , 
+        alternativas:[
+            
+        ]
+    }
+]

311933faad43470819bac82765b4e913c1960e75
bruna costa <166858058+amandaebruna11@users.noreply.github.com>
2025-09-04 16:23:15 -0300

    .

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-perguntas");
+const caixaPerguntas = document.querySelector(".caica-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultados = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".texto-resultado"); 
-
-const perguntas = [
-    {
-        enunciado: Quem deve ser responsável pela regulamentação da IA no futuro?
-            alternativas:[
-            Governos e entidades internacionais. 
-            Apenas as empresas que desenvolvem IA.  ],
-    },
-            enunciado:A IA deve ser desenvolvida com foco em quê?
-            alternativas:[
-                Melhorar a vida humana e promover o bem-estar.
-                Substituir completamente o trabalho humano.
-            ],
-
-    }
-]
+const caixaResultado = document.
